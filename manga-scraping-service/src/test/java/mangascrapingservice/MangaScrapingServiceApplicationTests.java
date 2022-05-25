77715178a010ebe2d7d54046b215edfb0d9272f5
Patu11 <patryk.t.warchol@gmail.com>
2022-05-25 10:14:33 +0200

    initial commit

---
+++ manga-scraping-service/src/test/java/mangascrapingservice/MangaScrapingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mangascrapingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaScrapingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
